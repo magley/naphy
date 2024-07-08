b8bfb3163cf4c7203be8ae916c25070d492e756a
magley <magazinlazar2@gmail.com>
2024-07-08 12:22:09 +0200

    feat: Add more tests (curtain, performance)

--- src/test/test2.cpp
+++ src/test/test2.cpp
@@ -1,4 +1,5 @@
 #include "test/test2.h"
+#include "game/gamescene.h"
 #include "naphy/physbody.h"
 #include <sstream>
 #include <string>
@@ -33,11 +34,11 @@ void gui_common(GameScene *scn) {
     cb->reg_toggle_target(&scene->debug_draw_shapes, true);
 
     cb          = gui->add(new GUICheckBox(gui, {24, 24}, "Draw Arbiter"));
-    cb->checked = true;
+    cb->checked = false;
     cb->reg_toggle_target(&scene->debug_draw_arbiters, true);
 
     cb          = gui->add(new GUICheckBox(gui, {48, 24}, "Draw QuadTree"));
-    cb->checked = true;
+    cb->checked = false;
     cb->reg_toggle_target(&scene->debug_draw_quadtree, true);
 
     cb          = gui->add(new GUICheckBox(gui, {72, 24}, "Draw Spring"));
@@ -86,17 +87,15 @@ void test_boxes_stacking(GameScene *scn) {
 
     // Floor
 
-    float box_size = 96;
-
-    b = scene->add(new PhysBody(Vec2(VIEW_W2 / 2.0, VIEW_H2 - 16), Shape(Vec2(1200, 64))));
+    b = scene->add(new PhysBody(Vec2(VIEW_W2 / 2.0, VIEW_H2 - 48), Shape(Vec2(1200, 64))));
     b->calc_mass(0);
 
     // Boxes
 
     for (int i = 0; i < 3; i++) {
-
-        float x = VIEW_W2 / 2.0;
-        float y = (VIEW_H2 - box_size / 2) - ((box_size + box_size / 3) * (i + 1));
+        float box_size = 96;
+        float x        = VIEW_W2 / 2.0;
+        float y        = (VIEW_H2 - 48) - ((box_size + 24) * (i + 1));
 
         b = scene->add(new PhysBody(Vec2(x, y), Shape(Vec2(box_size, box_size))));
     }
@@ -165,12 +164,100 @@ void test_rolling(GameScene *scn) {
     b = scene->add(new PhysBody(Vec2(VIEW_W2 / 2.0 + 290, 0), Shape(24.0f)));
 }
 
+void test_springs(GameScene *scn) {
+    PhysScene *scene = scn->physscene;
+    GUI       *gui   = scn->gui;
+
+    scene_clear_ext(scn, {0, 981}, 1);
+    PhysBody *b = nullptr;
+
+    float PARAM_DIST = 0;
+    float PARAM_K    = 50;
+    float PARAM_DAMP = 100;
+
+    {
+        // Curtain: grid of small bodies connected with springs
+
+        const unsigned                       bw = 15;
+        const unsigned                       bh = 10;
+        std::vector<std::vector<PhysBody *>> curtain(bh, std::vector<PhysBody *>(bw, NULL));
+
+        for (unsigned y = 0; y < bh; y++) {
+            for (unsigned x = 0; x < bw; x++) {
+                curtain[y][x] = new PhysBody(Vec2(300 + 20 * x, 160 + 16 * y), Shape(4));
+                scene->add(curtain[y][x]);
+
+                if (y == 0) {
+                    curtain[y][x]->calc_mass(0);
+                }
+            }
+        }
+        for (unsigned y = 0; y < bh; y++) {
+            for (unsigned x = 0; x < bw; x++) {
+                if (y + 1 < bh)
+                    scene->spring.push_back(Spring(curtain[y][x], curtain[y + 1][x], PARAM_DIST, PARAM_K, PARAM_DAMP));
+                if (x + 1 < bw)
+                    scene->spring.push_back(Spring(curtain[y][x], curtain[y][x + 1], PARAM_DIST, PARAM_K, PARAM_DAMP));
+            }
+        }
+    }
+}
+
+void test_performance(GameScene *scn) {
+    PhysScene *scene = scn->physscene;
+    GUI       *gui   = scn->gui;
+
+    scene_clear_ext(scn, {0, 981}, 1);
+    PhysBody *b = nullptr;
+
+    float PARAM_DIST = 1;
+    float PARAM_K    = 1000;
+    float PARAM_DAMP = 100;
+
+    // Ground
+
+    b = scene->add(new PhysBody(Vec2(8, VIEW_H2 / 2.0), Shape(Vec2(16, VIEW_H2))));
+    b->calc_mass(0);
+    b = scene->add(new PhysBody(Vec2(VIEW_W2 - 8, VIEW_H2 / 2.0), Shape(Vec2(16, VIEW_H2))));
+    b->calc_mass(0);
+    b = scene->add(new PhysBody(Vec2(VIEW_W2 / 2.0, VIEW_H2 - 8), Shape(Vec2(VIEW_W2, 16))));
+    b->calc_mass(0);
+
+    // Balls
+
+    GUIButton *btn;
+    btn = gui->add(new GUIButton(gui, {VIEW_W2 - 128.0, 48}, "Add 50 balls"));
+    btn->reg_click_callback([](GUIButton *btn) {
+        PhysScene *scene = btn->gui->scene->physscene;
+
+        int K         = 50;
+        int ball_size = 16;
+
+        int L = 32;
+        int R = VIEW_W2 - 32;
+        int U = 32;
+        int D = VIEW_H2 - 32;
+
+        int balls_per_row = (R - L) / (int)(ball_size * 1);
+
+        for (int i = 0; i < K; i++) {
+            int row = i / balls_per_row;
+            int col = i % balls_per_row;
+
+            int x = L + col * (ball_size * 1) + (3 * i / balls_per_row);
+            int y = U + row * (ball_size * 1);
+
+            scene->add(new PhysBody(Vec2(x, y), Shape(ball_size)));
+        }
+    });
+}
+
 void test2(GameScene *gamescene) {
     gamescene->physscene->clear();
     gamescene->gui->clear();
 
     static std::vector<void (*)(GameScene *)> init_func_arr = {
-        test_boxes_stacking, test_mass_proportions, test_rolling};
+        test_boxes_stacking, test_mass_proportions, test_rolling, test_springs, test_performance};
 
     if (scene_index >= init_func_arr.size())
         scene_index = 0;
